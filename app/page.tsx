b9e12ad5c73281bd46b137db395ea09e03c6b1f6
OliWebDevO <oliver-vdb@hotmail.com>
2024-09-20 17:02:42 +0200

    commit 2

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,16 @@
-import Hero from "@/components/hero";
+import Hero from "@/components/Hero";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+
       <div className="max-w-7xl w-full">
+
         <Hero />
 
       </div>
+      
     </main>
   );
 }

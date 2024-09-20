e33c542d10dfabcd4c0a629f6c22e5d9ffc0d6dd
OliWebDevO <oliver-vdb@hotmail.com>
2024-09-20 18:57:17 +0200

    Hero done

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import { Spotlight } from './ui/spotlight'
+import { TextGenerateEffect } from './ui/TextGenerateEffect'
+import MagicButton from './ui/MagicButton'
+import { FaLocationArrow } from 'react-icons/fa'
 
 const Hero = () => {
   return (
@@ -10,10 +13,31 @@ const Hero = () => {
             <Spotlight className='top-10 left-full h-[80vh] w-[50vw]' fill='purple'/>
             <Spotlight className='top-28 left-80 h-[80vh] w-[50vw]' fill='blue'/>
         </div>
-        <div className="h-screen w-full dark:bg-black-100 bg-white  dark:bg-grid-white/[0.3] bg-grid-black/[0.2] relative flex items-center justify-center">
+        <div className="h-screen w-full dark:bg-black-100 bg-white  dark:bg-grid-white/[0.03] bg-grid-black/[0.2] flex items-center justify-center absolute top-0 left-0">
             {/* Radial gradient for the container to give a faded look */}
             <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"/>
-           
+        </div>
+        <div className='flex justify-center relative my-20 z-10'>
+            <div className='max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center'>
+                <h2 className='uppercase tracking-widest text-xs text-center text-blue-100 max-w-80'>
+                    Dynamic Web Magic with Next.js
+                </h2>
+                <TextGenerateEffect 
+                    words='Transforming Concepts into Seamless User Experiences'
+                    className='text-[40px] md:text-5xl lg:text-6xl text-center'
+                />
+                <p className='text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl'>
+                    I am a Front-End web developer with a passion for creating dynamic web applications. 
+                    I specialize in building web applications with Next.js, React, and Tailwind CSS.
+                </p>
+                <a href="">
+                    <MagicButton 
+                    title='Get in Touch'
+                    icon={<FaLocationArrow />}
+                    position='right'
+                    />
+                </a>
+            </div>
         </div>
     </div>
   )
